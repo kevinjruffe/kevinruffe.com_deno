820134c4ec362f41c404d50c2e72ed4d24e790e6
Kevin Ruffe <kevinjruffe@gmail.com>
2022-09-29 22:39:07 -0400

    Fixes bug w/ date formatting

--- generator/newPost.ts
+++ generator/newPost.ts
@@ -35,7 +35,7 @@ const newPostFile = `blog/src/${date.toISOString()}_${urlEncodedTitle}.md`;
 // Create the file using title as heading, followed by date.
 Deno.writeTextFileSync(
   newPostFile,
-  `# [${rawTitle}](/${urlEncodedTitle})\n<div class=\"post-date\">"${dateLocaleString}"</div>`,
+  `# [${rawTitle}](/${urlEncodedTitle})\n<div class=\"post-date\">${dateLocaleString}</div>`,
 );
 
 console.log(green("\nFile created!\n"));
